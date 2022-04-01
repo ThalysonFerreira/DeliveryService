a58d5f34e64088d3c067099c61240b29ee7965eb
Thalyson Ferreira Chagas <thalyson.chagas@fitbank.com.br>
2022-04-01 11:18:15 -0300

    Criação Api

---
+++ Repository/Package.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testes.Repository
+{
+    public class Package
+    {
+        public long BarCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        
+    }
+}
